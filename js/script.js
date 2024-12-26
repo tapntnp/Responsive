19e97f9c00e10568746ad59f0e74c46682a2ea21
tanapat <72448619+tapntnp@users.noreply.github.com>
2024-12-26 13:29:35 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// เรียกใช้ jquery
+
+
+$(document).ready(function() {
+  $('.hamburger').on("click", function(){
+    $(".nav-grid").toggleClass("open");
+  });
+});
+
+function myFunction(x) {
+    x.classList.toggle("change");
+  }
